75c105499de5819019595a9c3d3513e1cca02539
jrobble <jrobble@mitre.org>
2012-01-23 15:53:48 -0500

    Updated build script.

--- Olsrd/scripts/build.sh
+++ Olsrd/scripts/build.sh
@@ -12,11 +12,18 @@ export BISON_PKGDATADIR=$aosp_path/external/bison/data
 source $aosp_path/build/envsetup.sh
 
 make OS=android DEBUG=1 build_all
+success=$?
 
-if [ $? == 0 ]; then
+# copy binary into other project(s)
+# copy will fail if destination dir does not exist
+cp olsrd ../../android-manet-service/AndroidManetService/res/raw
+
+if [ $success == 0 ]; then
   echo "Build successful"
   exit 0
 else
   echo "Build failed"
   exit 2
 fi
+
+
